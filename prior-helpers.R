6121b2079c38282c4539a6c646820e8c971dabb9
nshahpazov <nshahpazov@gmail.com>
2021-01-28 23:21:15 +0200

    added the 25 features of all combinations of heroes win\lose ratios. sometimes win, sometimes liun

--- prior-helpers.R
+++ prior-helpers.R
@@ -1,10 +1,3 @@
-generate_combinations <- function (hero_matches_df) {
-  crossing(hero_team_1 = 0:112, hero_team_2 = 0:112)  %>%
-    filter(hero_team_1 != hero_team_2) %>%
-    left_join(., count(hero_matches_df, hero_team_1, hero_team_2)) %>%
-    mutate(n = replace_na(n, 1))
-}
-
 generate_ratios_df <- function (team_heroes_df) {
   winners_hero_freq_df <- team_heroes_df %>%
     filter(radiant_win) %>%
@@ -17,4 +10,20 @@ generate_ratios_df <- function (team_heroes_df) {
   winners_hero_freq_df %>%
     mutate(ratio = n / losers_hero_freq_df$n) %>%
     select(-n)
+}
+
+generate_team_heroes_df <- function (players_df) {
+  players_df %>%
+    left_join(select(matches_df, match_id, radiant_win), by = "match_id") %>%
+    mutate(team = ifelse(player_slot < 6, "hero_team_1", "hero_team_2")) %>%
+    select(match_id, team, hero_id, radiant_win) %>%
+    pivot_wider(names_from = team, values_from = hero_id) %>%
+    unchop(everything())
+}
+
+generate_combinations <- function (hero_matches_df) {
+  crossing(hero_team_1 = 0:112, hero_team_2 = 0:112)  %>%
+    filter(hero_team_1 != hero_team_2) %>%
+    left_join(count(hero_matches_df, hero_team_1, hero_team_2)) %>%
+    mutate(n = replace_na(n, 1))
 }
